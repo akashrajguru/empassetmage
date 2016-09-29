8f6db2fd7c49f7dd4515476b8a5820f8af0336c2
Akash Rajguru <akashrajguru@icloud.com>
2016-09-29 15:14:50 +0100

    some changes

--- app.py
+++ app.py
@@ -8,17 +8,38 @@ def post_asset(emp_name, emp):
 	exists = emp_name in EMPS
 	emp['name'] = emp_name
 	if exists:
-		logging.info('Updating employee %s...', emp_name)
-		EMPS[emp_name].update(emp)
+		logging.info('Employee Record exists %s...', emp_name)
+		return 'Record exist please use PUT request', 409
 	else:
 		logging.info('Creating employee %s...', emp_name)
-		emp['taken_at'] = datetime.datetime.utcnow()
+		emp['taken_at'] = datetime.datetime.now()
 		EMPS[emp_name] = emp
-	return emp, (409 if exists else 201) 
+	return emp, 201
 
 def get_emps(name=None):
 	return [emp for emp in EMPS.values() if not name or emp['name'] == name]
 	
+def delete_emp(emp_name):
+	if emp_name in EMPS:
+		logging.info('Deleting Employee %s..', emp_name)
+		del EMPS[emp_name]
+		return 'Employee record deleted', 204
+	else:
+		return 'Employee record does not exist.', 404
+
+def put_emp(emp_name, emp):
+	exists = emp_name in EMPS
+	emp['name']=emp_name
+	if exists:
+		logging.info('Updating pet %s..', emp_name)
+		EMPS[emp_name].update(emp)
+		return 'Record updated ', 200
+	else:
+		logging.info('Record does not exists %s..', emp_name)
+		return 'Not found', 404
+
+
+
 
 
 
