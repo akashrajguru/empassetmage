e528e8d271030747d1558589dcb118eede5c0ac7
Akash Rajguru <akashrajguru@icloud.com>
2016-10-26 17:22:57 +0100

    commit for jenkins5 start.sh updated

--- start.sh
+++ start.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#fuser -k -n tcp 9090 && \
+#python app.py
 
-fuser -k -n tcp 9090 && \
-python app.py
+retfunce()
+{
+	fuser -k -n tcp 9090
+	echo "process killed"
+}
+
+exitfunc()
+{
+	python app.py
+	echo "application started"
+}
+
+retfunce
+exitfunc
