214bd2826311f8acf723e161a54b57b480474856
Akash Rajguru <akashrajguru@icloud.com>
2016-10-27 11:42:01 +0100

    new start code

--- start.sh
+++ start.sh
@@ -2,17 +2,32 @@
 #fuser -k -n tcp 9090 && \
 #python app.py
 
-
-retfunce()
-{
+retfunce(){
 	fuser -k -n tcp 9090
 	echo "process killed"
 }
 
-exitfunc()
-{
-	python app.py
-	echo "application started"
+# Install supervisor configs
+#if [ ! -f "/bin/supervisord.conf" ]; then 
+#	echo_supervisord_conf > /bin/supervisord.conf; 
+#fi
+
+# add this program to the supervisor configs
+#echo '
+#[program:app]
+#command=bash -c "python app.py"
+#' >> /etc/supervisord.conf
+
+
+# if supervisor already running, 'update' to add the new process
+#pgrep supervisord > /dev/null || supervisord
+#supervisorctl update
+
+
+
+exitfunc(){
+	nohup python app.py &
+	echo "application started" 
 }
 
 retfunce
